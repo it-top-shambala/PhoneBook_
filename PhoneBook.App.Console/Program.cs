9c69b1ef27fafd0fa6c591ba10dd1f8a01b73e7b
Starinin Andrey <starininandrey@gmail.com>
2023-11-25 12:32:54 +0300

    Добавление  ручного тестирования.

--- PhoneBook.App.Console/Program.cs
+++ PhoneBook.App.Console/Program.cs
@@ -1,3 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using PhoneBook.DbLib;
 
-Console.WriteLine("Hello, World!");
+var builder = new ConfigurationBuilder();
+builder.SetBasePath(Directory.GetCurrentDirectory());
+builder.AddJsonFile("appsettings.json");
+var config = builder.Build();
+var connectionString = config.GetConnectionString("DefaultConnection");
+
+var db = new PhoneBookContext(connectionString);
+/*var person = new Person()
+{
+    LastName = "Starinin",
+    FirstName = "Andrey"
+};
+
+var phone1 = new Phone()
+{
+    Type = PhoneType.Mobile,
+    Number = "+79042144491",
+    Person = person
+};
+var phone2 = new Phone()
+{
+    Type = PhoneType.Home,
+    Number = "+7 473 257-57-55",
+    Person = person
+};
+
+db.Phones.Add(phone1);
+db.Phones.Add(phone2);
+
+db.Persons.Add(person);
+db.SaveChanges();*/
+
+var persons = db.Persons.Include(person => person.Phones).ToList();
+foreach (var person in persons)
+{
+    Console.WriteLine($"{person.LastName} {person.FirstName}:");
+    var phones = person.Phones.ToList();
+    foreach (var phone in phones)
+    {
+        Console.WriteLine($"\t\t{phone.Type.ToString()} {phone.Number}");
+    }
+}
